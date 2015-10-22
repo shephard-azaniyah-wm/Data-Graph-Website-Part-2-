9001eff20b57330d7927fa9790affb7b7af7a6fc
Azaniyah Shephard <session1@START-Coding-MBP-22.local>
2015-10-22 14:51:42 -0700

    For some reason when I send a link from Animal Intake.html and send it to the same address it shows my second Graph. I am not sure why but I have taken advantage of this to show you.

--- Graph2.js
+++ Graph2.js
@@ -1,17 +1,16 @@
 google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ['Year', 'Sales', 'Expenses', 'Profit'],
-        ['2014', 1000, 400, 200],
-        ['2015', 1170, 460, 250],
-        ['2016', 660, 1120, 300],
-        ['2017', 1030, 540, 350]
+        ['Year', 'Dogs', 'Cats', 'Birds'],
+        ['2011',245, 161, 17],
+        ['2012', 6, 0, 0],
+        ['2013', 8, 19, 12]
     ]);
 
     var options = {
         chart: {
-            title: 'Company Performance',
-            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+            title: 'EValley',
+            subtitle: 'EValley Intake: 2011-13'
         },
         bars: 'vertical',
         vAxis: {format: 'decimal'},
