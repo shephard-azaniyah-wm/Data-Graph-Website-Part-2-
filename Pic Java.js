5ec029856983d5cca350a7bc8353d17ec9abc951
Azaniyah Shephard <session1@START-Coding-MBP-22.local>
2015-10-23 15:17:38 -0700

    Added in the 3 by 3 pictures

---
+++ Pic Java.js
@@ -0,0 +1,14 @@
+/**
+ * Created by session1 on 10/23/15.
+ */
+
+
+var main = function() {
+    $('#L1').click(function () {
+         $('#L1').attr('src', 'https://www.petsmartcharities.org/sites/default/files/styles/large/public/Homeless-cats_SM.jpg?itok=koG44WPh');
+    });
+};
+
+
+
+$(document).ready(main);
