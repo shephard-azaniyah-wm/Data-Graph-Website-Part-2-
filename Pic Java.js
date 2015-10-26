6896c7d570e51356ccc4f5a044e26450942df6df
shephard-azaniyah-wm <azaniyah.shephard@west-mec.org>
2015-10-25 23:29:14 -0700

    Update Pic Java.js

--- Pic Java.js
+++ Pic Java.js
@@ -5,10 +5,39 @@
 
 var main = function() {
     $('#L1').click(function () {
-         $('#L1').attr('src', 'https://www.petsmartcharities.org/sites/default/files/styles/large/public/Homeless-cats_SM.jpg?itok=koG44WPh');
+         $('#L1').attr('src', 'http://www.petango.com/sms/photos/255/a17b9bd5-f5bc-4845-ad27-7e81426bb1d2.jpg');
     });
+
+          $('#M1').click(function () {
+              $('#M1').attr('src', 'http://www.petango.com/sms/photos/255/fbcc8d6f-bb97-48b5-81aa-de9958a375e5.jpg');
+          });
+
+                   $('#R1').click(function () {
+                        $('#R1').attr('src', 'http://www.petango.com/sms/photos/255/58b0de9b-2398-4c83-a5b3-b0999ce76a1a.jpg');
+                   });
+
+    $('#L2').click(function () {
+        $('#L2').attr('src','http://www.petango.com/sms/photos/255/e624cd63-51bd-4ae3-9221-1b7a2fb42e43.jpg');
+    });
+
+         $('#M2').click(function (){
+              $('#M2').attr('src', 'http://www.petango.com/sms/photos/255/7a6a1a51-2d13-4e80-9863-a66db0c04a36.jpg');
+         });
+
+                  $('#R2').click(function () {
+                       $('#R2').attr('src', 'http://www.petango.com/sms/photos/255/68569c09-b79e-4189-b25e-2f6e2e446087.jpg');
+                  });
+
+    $('#L3').click(function () {
+        $('#L3').attr('src', 'http://www.petango.com/sms/photos/255/324a3689-af81-46e3-9593-569e1f517a83.jpg');
+    });
+
+        $('#M3').click(function () {
+             $('#M3').attr('src', 'http://www.petango.com/sms/photos/255/9e288828-66a2-49c7-bdac-1590acd2e1c5.jpg');
+        });
+
+                 $('#R3').click(function () {
+                      $('#R3').attr('src', 'http://www.petango.com/sms/photos/255/0646e9bf-4893-4a40-b6bc-38e7373b40b9.jpg');
+                 });
+
 };
-
-
-
-$(document).ready(main);
